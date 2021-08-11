de1d40ec0304df4614b06a932c793ef71419d85b
Thomas <thomas@Mac-mini-de-Thomas.local>
2021-08-11 11:25:19 +0200

    Added infos and API doc in Repair entity

--- src/repairs/repair.entity.ts
+++ src/repairs/repair.entity.ts
@@ -1,7 +1,6 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn, OneToOne, ManyToOne, JoinColumn } from "typeorm";
 import { ApiProperty } from "@nestjs/swagger";
 import { Scooter } from "src/scooters/scooter.entity";
-
 
 @Entity("repair")
 export class Repair {
@@ -9,19 +8,31 @@ export class Repair {
     @PrimaryGeneratedColumn({ type: "int" })
     id: number;
 
-    @ApiProperty()
+    @ApiProperty({
+        description: 'The name of the repair',
+        type: 'String'
+      })
     @Column({ type: "varchar", length: 25 })
     shortname: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        description: 'Description of the problem',
+        type: 'String'
+      })
     @Column({ type: "varchar", length: 255 })
     description: string;
 
-    @ApiProperty()
-    @Column()
+    @ApiProperty({
+        description: 'Price of the repair',
+        type: 'Number'
+    })
+    @Column({ type: "int" })
     price: number;
 
-    @ApiProperty()
+    @ApiProperty({
+            description: 'Id of the scooter that is related to the repair',
+            type: 'Number'
+        })
     @Column()
     scooterId: number;
 
